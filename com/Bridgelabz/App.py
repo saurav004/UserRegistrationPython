9f2bced837175ccb0579600bc8d8c3df9a922525
saurav004 <logtosaurav@gmail.com>
2020-11-30 21:15:29 +0530

    Added:last name is validated

--- com/Bridgelabz/App.py
+++ com/Bridgelabz/App.py
@@ -14,7 +14,17 @@ class Validator:
             print("Invalid First Name !!!")
             Validator.collect_and_validate_first_name()
 
+    @classmethod
+    def collect_and_validate_last_name(cls):
+        last_name = input("Enter last Name : ")
+        if re.fullmatch(Validator.FIRST_AND_LAST_NAME_PATTERN, last_name):
+            print(f"Last Name validated, HI {last_name}")
+        else:
+            print("Invalid Last Name !!!")
+            Validator.collect_and_validate_last_name()
+
 
 if __name__ == "__main__":
     print("Welcome to User Registration Program")
     Validator.collect_and_validate_first_name()
+    Validator.collect_and_validate_last_name()

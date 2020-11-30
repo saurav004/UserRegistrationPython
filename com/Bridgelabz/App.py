86b2320291ac541294d747c7d6727ba096bdd062
saurav004 <logtosaurav@gmail.com>
2020-11-30 21:51:15 +0530

    Added:User's Email ID is validated

--- com/Bridgelabz/App.py
+++ com/Bridgelabz/App.py
@@ -2,11 +2,15 @@ import re
 
 
 class Validator:
-
     FIRST_AND_LAST_NAME_PATTERN = "^[A-Z]{1}[a-zA-Z]{2,30}$"
+    EMAIL_PATTERN = "^[a-zA-Z][a-zA-Z0-9_\\-+]*[.]{0,1}[a-zA-Z0-9_\\-+]{1,}[@][a-zA-Z0-9]{1,}[.][a-zA-Z]{2,}[.]{0,}[a-zA-Z]*$"
 
     @classmethod
     def collect_and_validate_first_name(cls):
+        """
+        Objective:taking first name and matching it with respective pattern
+        :rtype: void
+        """
         first_name = input("Enter first Name : ")
         if re.fullmatch(Validator.FIRST_AND_LAST_NAME_PATTERN, first_name):
             print(f"First Name validated, HI {first_name}")
@@ -16,15 +20,33 @@ class Validator:
 
     @classmethod
     def collect_and_validate_last_name(cls):
+        """
+        Objective:taking last name and matching it with respective pattern
+        :rtype: void
+        """
         last_name = input("Enter last Name : ")
         if re.fullmatch(Validator.FIRST_AND_LAST_NAME_PATTERN, last_name):
-            print(f"Last Name validated, HI {last_name}")
+            print(f"Last Name validated")
         else:
             print("Invalid Last Name !!!")
             Validator.collect_and_validate_last_name()
+
+    @classmethod
+    def collect_and_validate_email(cls):
+        """
+        Objective:taking email Id and matching it with respective pattern
+        :rtype: void
+        """
+        email = input("Enter Email ID : ")
+        if re.fullmatch(Validator.EMAIL_PATTERN, email):
+            print(f"Email Validated")
+        else:
+            print("Invalid Email !!!")
+            Validator.collect_and_validate_email()
 
 
 if __name__ == "__main__":
     print("Welcome to User Registration Program")
     Validator.collect_and_validate_first_name()
     Validator.collect_and_validate_last_name()
+    Validator.collect_and_validate_email()
